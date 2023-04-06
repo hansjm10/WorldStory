f90ca95c557df5cf2fa11b0d7c54af8e64cedc2a
Jordan Hans <jordan.hans@live.com>
2023-04-05 23:10:09 -0500

    Add in choosing Alignment.

---
+++ DnDWorldCreate/Shared/ListComponents/EnumListComponentBase.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Components;
+
+public abstract class EnumListComponentBase<TEnum> : ComponentBase where TEnum : Enum
+{
+    [Parameter] public string Label { get; set; }
+    [Parameter] public EventCallback<TEnum> SelectedValueChanged { get; set; }
+    [Parameter] public IReadOnlyList<TEnum> EnumValues { get; set; } = new List<TEnum>();
+    [Parameter] public TEnum SelectedValue { get; set; }
+    protected async Task OnSelect(ChangeEventArgs e)
+    {
+        if (Enum.TryParse(typeof(TEnum), e.Value.ToString(), out var result))
+        {
+            SelectedValue = (TEnum)result;
+            await SelectedValueChanged.InvokeAsync((TEnum)result);
+        }
+    }
+}
